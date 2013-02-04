4067141f2e499f1f2f60646070e0c5cf628d5631
manyak <manyak@zeenbook>
2013-02-05 02:47:32 +0400

    5.02.13

--- main.f90
+++ main.f90
@@ -1,6 +1,6 @@
 program main
     implicit none
-integer(4) i,j,k
+integer(4) i,j,k,kk
 integer(4) periodik
 real(8) ddir
 real(8) lobsh
@@ -9,6 +9,7 @@ real(8) hmemb
 real(8),allocatable:: memax(:),memay(:),memaz(:)
 character(2) me
 integer(4) N,nom
+integer(4) hperiodik
 
 me='A'
 periodik=8
@@ -16,23 +17,29 @@ ddir=1.15
 hmemb=float(periodik)*ddir
 lobsh=20.0
 lmemb=10.0
+hperiodik=3
 
-allocate(memax(2*periodik*periodik))
-allocate(memay(2*periodik*periodik))
-allocate(memaz(2*periodik*periodik))
+allocate(memax(2*periodik*periodik*hperiodik))
+allocate(memay(2*periodik*periodik*hperiodik))
+allocate(memaz(2*periodik*periodik*hperiodik))
 
 nom=0
-N=2*periodik*periodik
+N=2*periodik*periodik*hperiodik
 do i=1,2
     do j=1,periodik
         do k=1,periodik
+            do kk=1,hperiodik
             nom=nom+1
             memax(nom)=(float(j)-0.5)*ddir
             memay(nom)=(float(k)-0.5)*ddir
-            memaz(nom)=(float(i)-1.5)*lmemb
+            memaz(nom)=(float(i)-1.5)*lmemb+(float(kk)-1.0)*ddir
+            enddo
         enddo
     enddo
 enddo
+maxzdob=0.5*lmemb+(float(hperiodik)-1.0)*ddir+0.5*ddir
+
+
 
 open(7,file='membrane.gro')
 write(7,'(a)') 'membrane created with memcreat'
